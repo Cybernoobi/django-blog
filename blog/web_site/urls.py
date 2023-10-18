00b8012656f0e856ba7582e72ba50185cdf1f55d
student <student@intranet.proweb.uz>
2023-10-18 16:28:34 +0500

    added article detalis

--- blog/web_site/urls.py
+++ blog/web_site/urls.py
@@ -7,4 +7,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('categories/<str:category_id>/', views.category_articles, name='category_articles'),
+    path('articles/<str:article_id>/', views.article_detail, name='article_detail'),
 ]
